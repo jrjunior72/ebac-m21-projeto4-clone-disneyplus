2436170c4ffedf0d5aa2d557ee219e78e3d44eb2
jrjunior72 <jrjunior72@hotmail.com>
2024-10-19 19:02:36 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
 
+// importar o plugin do gulp-uglify
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
+
+// mimificar os arquivos
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -17,8 +26,9 @@ function images() {
         .pipe(gulp.dest('./dist/images'));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
-exports.watch = function(){
+exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
